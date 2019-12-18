0970cdb562a3a52e10ce912474154f98f0d743db
David Farquharson <david.farquharson@zebra.com>
2019-12-18 17:07:30 -0600

    all the cmake stuff (probably too much)

---
+++ cmake-build-debug/CMakeFiles/cpp_playground.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dfarquharson/dsjf/cpp-playground/cpp-in-y-minutes.cpp" "/Users/dfarquharson/dsjf/cpp-playground/cmake-build-debug/CMakeFiles/cpp_playground.dir/cpp-in-y-minutes.cpp.o"
+  "/Users/dfarquharson/dsjf/cpp-playground/dsjf-mock-playground.cpp" "/Users/dfarquharson/dsjf/cpp-playground/cmake-build-debug/CMakeFiles/cpp_playground.dir/dsjf-mock-playground.cpp.o"
+  "/Users/dfarquharson/dsjf/cpp-playground/dsjf-test-playground.cpp" "/Users/dfarquharson/dsjf/cpp-playground/cmake-build-debug/CMakeFiles/cpp_playground.dir/dsjf-test-playground.cpp.o"
+  "/Users/dfarquharson/dsjf/cpp-playground/main.cpp" "/Users/dfarquharson/dsjf/cpp-playground/cmake-build-debug/CMakeFiles/cpp_playground.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
