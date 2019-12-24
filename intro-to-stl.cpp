99442c78008d90f0daed6cfe1dc33e8f72877830
David Farquharson <david.s.j.farquharson@gmail.com>
2019-12-24 08:02:42 -0600

    messing with strings in the stl

--- intro-to-stl.cpp
+++ intro-to-stl.cpp
@@ -6,7 +6,10 @@ TEST_CASE("stl::Strings") {
     REQUIRE(s.length() == 5);
     REQUIRE(s.size() == 5);
     REQUIRE(s == "hello");
-    std::string t;
-    std::transform(s.begin(), s.end(), std::back_inserter(t), ::toupper);
-    REQUIRE(t == "HELLO");
+
+    SECTION("Uppercasing a String") {
+        std::string t;
+        std::transform(s.begin(), s.end(), std::back_inserter(t), ::toupper);
+        REQUIRE(t == "HELLO");
+    }
 }
